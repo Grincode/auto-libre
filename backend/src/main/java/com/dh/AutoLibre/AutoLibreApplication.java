ca07ae36e4ff058225b7cf2f4a1b0dfcb1cfdd4d
Juan Castañar <juanpetarcich@gmail.com>
2025-03-16 20:39:08 -0300

    Generación de archivos base react y spring

---
+++ backend/src/main/java/com/dh/AutoLibre/AutoLibreApplication.java
@@ -0,0 +1,13 @@
+package com.dh.AutoLibre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoLibreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoLibreApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dh/AutoLibre/AutoLibreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dh.AutoLibre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoLibreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
